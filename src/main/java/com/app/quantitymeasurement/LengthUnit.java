b335c08da2c0661e743846232a43291e1a7e8cf2
wasim-akaram <wasimakaram360@gmail.com>
2026-02-23 17:19:07 +0530

    [Wasim] : added the test script for targetUnitAddition and test script

--- src/main/java/com/app/quantitymeasurement/LengthUnit.java
+++ src/main/java/com/app/quantitymeasurement/LengthUnit.java
@@ -1,21 +1,19 @@
 package com.app.quantitymeasurement;
 
-public enum LengthUnit 
-{
-		FEET(12.0),      
-	    INCHES(1.0),
-	    YARDS(36.0),
-	    CENTIMETERS(0.393701 / 12.0);     
+public enum LengthUnit {
 
-	    private final double conversionFactor;
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
 
-	    LengthUnit(double conversionFactor) 
-	    {
-	        this.conversionFactor = conversionFactor;
-	    }
+    private final double conversionFactor;
 
-	    public double getConversionFactor() 
-	    {
-	        return conversionFactor;
-	    }
-	}
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+}
