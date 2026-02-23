7d3d2217795947e2eb6666a2f2477e86a90f23cc
wasim-akaram <wasimakaram360@gmail.com>
2026-02-23 12:09:51 +0530

    [Wasim] : added the inch class functionality of UC2 and aded test script for testing

--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,25 +1,32 @@
 package com.app.quantitymeasurement;
 
-import java.util.Scanner;
 public class QuantityMeasurementApp 
 {
+	public static boolean compareFeet(double v1, double v2) 
+	{
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+	public static boolean compareInches(double v1, double v2) 
+	{
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
+	
+	
     public static void main(String[] args) 
     {
-    	Scanner sc = new Scanner(System.in);
-    	
-    	Feet f1 = new Feet(sc.nextDouble());
-    	Feet f2 = new Feet(sc.nextDouble());
-    	
-    	if(f1.equals(f2))
-    	{
-    		System.out.println("Both the values are equal");
-    		
-    	}
-    	else
-    	{
-    		System.out.println("Both the values are not equal");
-    	}
-    	
+    	   boolean feetResult = compareFeet(1.0, 1.0);
+           boolean inchResult = compareInches(1.0, 1.0);
+
+           System.out.println("Input: 1.0 ft and 1.0 ft");
+           System.out.println("Output: Equal (" + feetResult + ")");
+
+           System.out.println("Input: 1.0 inch and 1.0 inch");
+           System.out.println("Output: Equal (" + inchResult + ")");
        
     }
 }
