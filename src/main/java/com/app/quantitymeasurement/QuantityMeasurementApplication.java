2d9a652a652fd3f4e7ac6549be0e0739a307c013
wasim-akaram <wasimakaram360@gmail.com>
2026-03-24 11:27:53 +0530

    [Wasim] : added some modification in class QuantityMeasurement Application

--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementApplication.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApplication.java
@@ -2,6 +2,7 @@ package com.app.quantitymeasurement;
 
 import org.springframework.boot.SpringApplication;
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
