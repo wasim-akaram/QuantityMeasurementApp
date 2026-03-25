8be1e43ec166d4c9f56e988d2ce56f57244b9bc3
wasim-akaram <wasimakaram360@gmail.com>
2026-03-25 16:29:05 +0530

    [Wasim] : removed unwanted files

--- src/main/java/com/app/dump/IQuantityMeasurementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.dump;
-
-import com.app.quantitymeasurement.model.QuantityMeasurementEntity;
-
-public interface IQuantityMeasurementRepository {
-
-    void save(QuantityMeasurementEntity entity);
-
-}
